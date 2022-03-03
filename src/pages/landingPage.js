50ebc5262a0638cbed337489afbaed64d733ff18
Nancy Rike <nancyrike7@gmail.com>
2022-03-04 00:13:10 +0100

    initial commit

---
+++ src/pages/landingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LandingPage = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default LandingPage
+
+const Container = styled.section``;
+const 
